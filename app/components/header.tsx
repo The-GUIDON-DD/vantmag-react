fd24263584ac56e6ca1867c6975c04d55811ed96
Cheska Huang <julyannahuang@gmail.com>
2026-09-14 00:54:16 +0800

    hide show search

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { CgSearch } from "react-icons/cg";
+import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router";
 
 const links = [
@@ -39,14 +42,18 @@ const links = [
   },
 ];
 
-function Search() {
+function Search({ hidden = true }: { hidden?: boolean }) {
   /** TODO: add in search bar **/
   return (
-    <section className="w-full flex justify-center items-center py-6 h-28 bg-[#dddffe]">
-      <section className="bg-white h-full rounded-full w-[50vw] px-10">
+    <section
+      className="w-full flex justify-center items-center py-6 h-28 bg-[#dddffe] transition-transform duration-500 z-5"
+      style={{ transform: hidden ? "translateY(-100%)" : "" }}
+    >
+      <section className="bg-white h-full rounded-full w-[50vw] px-5 flex items-center">
+        <CgSearch className="text-vant-purple text-3xl mr-5" />
         <input
           type="text"
-          className="bg-transparent border-none focus:outline-none focus:ring-0 w-full h-full"
+          className="bg-transparent border-none focus:outline-none focus:ring-0 w-full h-full text-2xl"
         />
       </section>
     </section>
@@ -54,9 +61,11 @@ function Search() {
 }
 
 export default function Header() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <header className="sticky w-screen flex flex-col items-stretch">
-      <section className="py-4 w-full flex flex-col justify-center items-center bg-vant-purple">
+      <section className="py-4 w-full flex flex-col justify-center items-center bg-vant-purple z-10">
         <Link to="/">
           <img
             src="/logo.svg"
@@ -64,22 +73,38 @@ export default function Header() {
             className="w-[15vw] mb-6"
           />
         </Link>
-        <nav>
-          <ul className="flex list-none text-white uppercase font-bold text-lg">
-            {links.map(({ title, path }, ix) => (
-              <li key={title}>
-                <Link to={path}>
-                  {title}
-                  {ix < links.length - 1 && (
-                    <span className="mx-4">&middot;</span>
-                  )}
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <nav className="flex grid-cols-3 gap-15">
+          <section className="col-start-2 justify-self-center">
+            <ul className="flex list-none text-white uppercase font-bold text-lg">
+              {links.map(({ title, path }, ix) => (
+                <li key={title}>
+                  <Link to={path}>
+                    {title}
+                    {ix < links.length - 1 && (
+                      <span className="mx-4">&middot;</span>
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <button
+            onClick={() => setShowSearch((prev) => !prev)}
+            type="button"
+            className="text-white text-2xl font-bold"
+          >
+            <CgSearch />
+          </button>
         </nav>
       </section>
-      <Search />
+      <Search hidden={!showSearch} />
+      <div
+        className="fixed z-2 h-screen w-screen bg-[rgba(0,0,0,0.5)] duration-350 transition-all"
+        style={{
+          opacity: showSearch ? 1 : 0,
+          pointerEvents: showSearch ? "auto" : "none",
+        }}
+      />
     </header>
   );
 }
